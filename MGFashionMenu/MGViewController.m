eb07d5fd34d434686cd416b8ab63a064b41edf00
Matteo Gobbi <job@matteogobbi.it>
2014-07-12 00:41:02 +0100

    added more example

--- MGFashionMenu/MGViewController.m
+++ MGFashionMenu/MGViewController.m
@@ -13,22 +13,30 @@
 @interface MGViewController ()
 
 - (IBAction)menuAction:(id)sender;
+- (IBAction)menuAction2:(id)sender;
+- (IBAction)menuAction3:(id)sender;
 
 @end
 
 
 @implementation MGViewController {
     MGMenuView *_menuView;
+    MGMenuView *_menuView2;
+    MGMenuView *_menuView3;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    UIView *exampleView = [self createExampleView];
-    
-    _menuView = [[MGMenuView alloc] initWithMenuView:exampleView];
+    _menuView = [[MGMenuView alloc] initWithMenuView:[self createExampleView]];
     [self.view addSubview:_menuView];
+    
+    _menuView2 = [[MGMenuView alloc] initWithMenuView:[self createExampleView2]];
+    [self.view addSubview:_menuView2];
+    
+    _menuView3 = [[MGMenuView alloc] initWithMenuView:[self createExampleView3]];
+    [self.view addSubview:_menuView3];
 }
 
 - (UIView *)createExampleView {
@@ -45,11 +53,54 @@
     return view;
 }
 
+- (UIView *)createExampleView2 {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    [view setBackgroundColor:[UIColor blackColor]];
+    
+    UILabel *lbl = [[UILabel alloc] initWithFrame:view.bounds];
+    [lbl setText:@"x"];
+    [lbl setFont:[UIFont boldSystemFontOfSize:16.0]];
+    [lbl setTextAlignment:NSTextAlignmentCenter];
+    [lbl setTextColor:[UIColor whiteColor]];
+    [view addSubview:lbl];
+    [lbl setCenter:view.center];
+    
+    return view;
+}
+
+- (UIView *)createExampleView3 {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 150)];
+    [view setBackgroundColor:[UIColor colorWithRed:0.2 green:0.722 blue:0.2 alpha:1]];
+    
+    UILabel *lbl = [[UILabel alloc] initWithFrame:view.bounds];
+    [lbl setText:@"This is a test"];
+    [lbl setTextAlignment:NSTextAlignmentCenter];
+    [lbl setTextColor:[UIColor whiteColor]];
+    [view addSubview:lbl];
+    [lbl setCenter:view.center];
+    
+    return view;
+}
+
 - (IBAction)menuAction:(id)sender {
     if (_menuView.isShown)
         [_menuView hide];
     else
         [_menuView show];
+}
+
+- (IBAction)menuAction2:(id)sender {
+    if (_menuView2.isShown)
+        [_menuView2 hide];
+    else
+        [_menuView2 show];
+}
+
+- (IBAction)menuAction3:(id)sender {
+    if (_menuView3.isShown)
+        [_menuView3 hide];
+    else
+        [_menuView3 show];
 }
 
 - (BOOL)prefersStatusBarHidden {
