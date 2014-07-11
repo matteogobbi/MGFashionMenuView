18b4ed3b803cac8da5e3685060c3f9cf80621dc1
Matteo Gobbi <job@matteogobbi.it>
2014-07-12 00:56:51 +0100

    supported multiple animation

--- MGFashionMenu/MGMenuView.h
+++ MGFashionMenu/MGMenuView.h
@@ -8,11 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, MGAnimationType) {
+    MGAnimationTypeBlob
+};
+
 @interface MGMenuView : UIView
 
 @property (atomic, assign, readonly) BOOL isShown;
+@property (atomic, assign, readonly) BOOL isAnimating;
 
 - (instancetype)initWithMenuView:(UIView *)menuView;
+- (instancetype)initWithMenuView:(UIView *)menuView andAnimation:(MGAnimationType)animationType;
 
 - (void)show;
 - (void)hide;
--- MGFashionMenu/MGMenuView.m
+++ MGFashionMenu/MGMenuView.m
@@ -11,23 +11,38 @@
 static CGFloat const sideHelperView = 20.0;
 static CGFloat const animationDuration = .5;
 
+
+struct Animation {
+    NSTimeInterval sideDelay;
+    NSTimeInterval sideDumping;
+    NSTimeInterval sideVelocity;
+    NSTimeInterval centerDelay;
+    NSTimeInterval centerDumping;
+    NSTimeInterval centerVelocity;
+};
+
+
 @implementation MGMenuView {
     UIView *_centerAnchorView;
     UIView *_sideAnchorView;
     UIView *_menuView;
     UIColor *_fillColor;
     CADisplayLink *_displayLink;
-    
-    BOOL _isAnimating;
+    struct Animation animation;
 }
 
+- (instancetype)init {
+    @throw [NSException exceptionWithName:@"Invalid init" reason:@"Use initViewMenuView: instead" userInfo:nil];
+}
+
+- (instancetype)initWithMenuView:(UIView *)menuView {
+    return [self initWithMenuView:menuView andAnimation:MGAnimationTypeBlob];
+}
 
 //Init the menuView and the helperViews
-- (instancetype)initWithMenuView:(UIView *)menuView {
+- (instancetype)initWithMenuView:(UIView *)menuView andAnimation:(MGAnimationType)animationType {
     
     CGRect fakeRect = menuView.frame;
-    
-    //from up
     
     //Go to make the under view bigger for the animation
     fakeRect.size.height += 25.0;
@@ -36,18 +51,22 @@ static CGFloat const animationDuration = .5;
         
         _menuView = menuView;
         
-        //from up
+        //Prepare views positions
         _sideAnchorView = [[UIView alloc] initWithFrame:CGRectMake(-sideHelperView/2.0, -sideHelperView/2.0, sideHelperView, sideHelperView)];
         
         _centerAnchorView = [[UIView alloc] initWithFrame:CGRectMake(self.frame.size.width/2.0-sideHelperView/2.0, -sideHelperView/2.0, sideHelperView, sideHelperView)];
         
-        _fillColor = menuView.backgroundColor;
-        
         _menuView.frame = CGRectMake(0, -_menuView.frame.size.height, _menuView.frame.size.width, _menuView.frame.size.height);
         
+        //Colors
+        _fillColor = menuView.backgroundColor;
         [self setBackgroundColor:[UIColor clearColor]];
         [_menuView setBackgroundColor:[UIColor clearColor]];
         
+        //Animation
+        [self mg_setValuesForAnimationType:animationType];
+        
+        //Flags
         _isAnimating = NO;
         _isShown = NO;
     }
@@ -110,7 +129,7 @@ static CGFloat const animationDuration = .5;
         [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
         
         
-        [UIView animateWithDuration:animationDuration delay:.1 usingSpringWithDamping:.4 initialSpringVelocity:.9 options:(UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionAllowUserInteraction) animations:^{
+        [UIView animateWithDuration:animationDuration delay:animation.sideDelay usingSpringWithDamping:animation.sideDumping initialSpringVelocity:animation.sideVelocity options:(UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionAllowUserInteraction) animations:^{
             
             if (needsToAppear) {
                 _sideAnchorView.frame = CGRectMake(_sideAnchorView.frame.origin.x, _menuView.frame.size.height-sideHelperView/2.0, sideHelperView, sideHelperView);
@@ -128,7 +147,7 @@ static CGFloat const animationDuration = .5;
         }];
         
         
-        [UIView animateWithDuration:animationDuration delay:0 usingSpringWithDamping:.6 initialSpringVelocity:.6 options:(UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionAllowUserInteraction) animations:^{
+        [UIView animateWithDuration:animationDuration delay:animation.centerDelay usingSpringWithDamping:animation.centerDumping initialSpringVelocity:animation.centerVelocity options:(UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionAllowUserInteraction) animations:^{
             
             if (needsToAppear)
                 _centerAnchorView.frame = CGRectMake(_centerAnchorView.frame.origin.x, _menuView.frame.size.height-sideHelperView/2.0, sideHelperView, sideHelperView);
@@ -146,6 +165,23 @@ static CGFloat const animationDuration = .5;
 //Called from the displayLink each frame of the view changes
 - (void)mg_tick:(CADisplayLink *)displayLink {
     [self setNeedsDisplay];
+}
+
+- (void)mg_setValuesForAnimationType:(MGAnimationType)animationType {
+    switch (animationType) {
+        case MGAnimationTypeBlob: {
+            animation.sideDelay = .1;
+            animation.sideDumping = .4;
+            animation.sideVelocity = .9;
+            animation.centerDelay = 0;
+            animation.centerDumping = .6;
+            animation.centerVelocity = .6;
+        }
+            break;
+            
+        default:
+            break;
+    }
 }
 
 @end
